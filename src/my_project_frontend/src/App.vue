4cd90278597d6f2b7424ddba88c98bf71b89a103
Dawid Szymczyk <keralis@o2.pl>
2025-03-04 18:55:48 +0000

    components

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -25,4 +25,21 @@ async function handleSubmit(e) {
     </form>
     <section id="greeting">{{ greeting }}</section>
   </main>
+  <div class="flex flex-col items-center justify-center gap-4 p-10">
+    <ICP />
+    <Calculator />
+  </div>
 </template>
+
+<script>
+import ICP from "./components/Icp.vue";
+import Calculator from "./components/Calculator.vue";
+
+export default {
+  components: {
+    ICP,
+    Calculator,
+  },
+};
+</script>
+
